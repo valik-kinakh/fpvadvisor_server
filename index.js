49438610656e5f6843aa7d87e16a4377cd4b292a
Valik Kinakh <valik.kinah@gmail.com>
2024-05-11 12:26:36 +0300

    add get endpoint

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 
 const app = express();
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 3030;
 
 app.use(cors());
 app.use(express.json());
@@ -32,57 +32,67 @@ const DroneSchema = new Schema({
 });
 const DroneModel = mongoose.model('drone', DroneSchema);
 
+app.get('/', (req, res) => {
+    return res.send('server works')
+})
+
 app.post('/drones/find', async (req, res) => {
-    const { operationType, flightType,
-        combatFlightDetails,
-        intelligenceFlightDetails,
-        whetherConditions,
-    } = req.body;
-    const { isNight } = whetherConditions;
+    try {
+        const { operationType, flightType,
+            combatFlightDetails,
+            intelligenceFlightDetails,
+            whetherConditions,
+        } = req.body;
+        const { isNight } = whetherConditions;
 
-    if (operationType === 'CombatFlight'){
-        const { distance, payload } = combatFlightDetails || {};
+        if (operationType === 'CombatFlight'){
+            const { distance, payload } = combatFlightDetails || {};
 
-        const result = await DroneModel.find({
-            purpose: operationType,
-            type: flightType,
-            distance: { $gte: distance },
-            payload: { $gte: payload },
-            isNight
-        })
-
-        if (result?.length === 0) {
-            const newResult = await DroneModel.find({
+            const result = await DroneModel.find({
                 purpose: operationType,
                 type: flightType,
+                distance: { $gte: distance },
+                payload: { $gte: payload },
                 isNight
-            }).limit(5);
+            })
 
-            return res.json(newResult);
-        }
+            if (result?.length === 0) {
+                const newResult = await DroneModel.find({
+                    purpose: operationType,
+                    type: flightType,
+                    isNight
+                }).limit(5);
 
-        return res.json(result)
+                return res.json(newResult);
+            }
 
-    }else if (operationType === 'Intelligence'){
-        const { distance, duration } = intelligenceFlightDetails || {};
+            return res.json(result)
 
-        const result = await DroneModel.find({
-            purpose: operationType,
-            distance: { $gte: distance },
-            flightTime: { $gte: duration },
-            isNight
-        })
+        }else if (operationType === 'Intelligence'){
+            const { distance, duration } = intelligenceFlightDetails || {};
 
-        if (result?.length === 0) {
-            const newResult = await DroneModel.find({
+            const result = await DroneModel.find({
                 purpose: operationType,
+                distance: { $gte: distance },
+                flightTime: { $gte: duration },
                 isNight
-            }).limit(5);
+            })
 
-            return res.json(newResult);
+            if (result?.length === 0) {
+                const newResult = await DroneModel.find({
+                    purpose: operationType,
+                    isNight
+                }).limit(5);
+
+                return res.json(newResult);
+            }
+
+            return res.json(result)
         }
-
-        return res.json(result)
+    }catch (e) {
+        return res.status(400).json({
+            message: e?.message
+        })
     }
 })
 
